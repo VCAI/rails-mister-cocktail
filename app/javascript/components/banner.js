e990fc99af4464ec972060b19863eed899c56377
Velina Cai <velina_30@yahoo.com>
2018-02-24 13:39:18 +0000

    myx

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
     strings: ["Mix Your Ingredients", "Create New Cocktails"],
-    typeSpeed: 70,
+    typeSpeed: 120,
     loop: true
   });
 }
